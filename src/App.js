5ede96edc6d6fe2e0da549e87fa7940c269eafea
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-20 14:56:51 -0400

    add entry form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
-
+import AddEntryForm from './Components/DisplayEntries/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
+ 
+  function addNewEntry(entry){
+   let tempEntries =[...entries, entry];
+
+   setEntries(tempEntries);
+ }
+  
   return (
     <div>
       <DisplayEntries parentEntries={entries}/>
-
+      <AddEntryForm addNewEntryProperty={addNewEntry} />
     </div>
   );
 }
